880e9fc3cf2c6100728dc5774a7af9141d74f0b9
Jon-Michael Deldin <dev@jmdeldin.com>
2012-02-18 15:25:28 -0700

    Update roster

--- _plugins/roster_tag.rb
+++ _plugins/roster_tag.rb
@@ -2,6 +2,11 @@ require 'csv'
 
 module Jekyll
   class RosterTag < Liquid::Tag
+    URLS = {
+      'GitHub' => 'https://github.com',
+      'LinkedIn' => 'https://linkedin.com/in',
+    }
+
     def initialize(tag_name, text, tokens)
       super
       @filename = File.join(File.dirname(__FILE__), '../files/roster.csv')
@@ -20,13 +25,30 @@ module Jekyll
       s.join("\n")
     end
 
+    private
+
     def render_row(row)
       s = %w(<tr>)
       row.each do |field, value|
-        s << '<td class="%s">%s</td>' % [field, value]
+        field.strip!
+
+        s << '<td class="%s">' % field
+
+        if value && url = URLS[field]
+          s << render_link_template(field, url, value.strip)
+        elsif field == 'Homepage' && value
+          s << render_link_template(field, value, '')
+        else
+          s << value
+        end
+        s << '</td>'
       end
       s << '</tr>'
       s.join
+    end
+
+    def render_link_template(text, base, var)
+      '<a href="%s" rel="external">%s</a>' % [File.join(base, var), text]
     end
   end
 end
--- pages/roster.md
+++ pages/roster.md
@@ -3,15 +3,32 @@ layout: static
 title: Roster
 ---
 
-If you would like to be included on this list, please send Jon-Michael an
-email with your name, <tt>@umontana</tt> prefix
-(e.g., <tt>jon-michael.deldin</tt>), status (junior, senior, grad, etc.),
-GitHub username, and whether you are available for contract work.
+We're trying to build a roster, so we'd appreciate it if you would send
+Jon-Michael an email with your
+
+- name
+- <tt>@umontana</tt> prefix (e.g., <tt>jon-michael.deldin</tt>)
+- class (junior, senior, grad, etc.)
+- GitHub username
+- LinkedIn profile
+- available for contract work?
 
 <table>
   <tr>
     <th>
       Name
+    </th>
+    <th>
+      GitHub
+    </th>
+    <th>
+      LinkedIn
+    </th>
+    <th>
+      Homepage
+    </th>
+    <th>
+      Contract work?
     </th>
   </tr>
   {% roster %}
